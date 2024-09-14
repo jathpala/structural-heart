016c43e91cb1f75c70d365198bff9a417762429f
Jath Palasubramaniam <jathpala@gmail.com>
2024-09-14 16:33:16 +1000

    Improved patient detail reset functionality

--- src/lib/PatientDetails.svelte
+++ src/lib/PatientDetails.svelte
@@ -10,69 +10,103 @@ export let generalPractitioner = ""
 export let primaryCardiologist = ""
 export let structuralCardiologist = ""
 export let status = "assess"
+
+const originalMrn = mrn
+const originalFirstname = firstname
+const originalLastname = lastname
+const originalSex = sex
+const originalDob = dob
+const originalGeneralPractitioner = generalPractitioner
+const originalPrimaryCardiologist = primaryCardiologist
+const originalStructuralCardiologist = structuralCardiologist
+const originalStatus = status
+
+let disabled = !!mrn    // Converts string to boolean
+
+function unlock() {
+    disabled = false
+}
+
+function reset() {
+    mrn = originalMrn
+    firstname = originalFirstname
+    lastname = originalLastname
+    sex = originalSex
+    dob = originalDob
+    generalPractitioner = originalGeneralPractitioner
+    primaryCardiologist = originalPrimaryCardiologist
+    structuralCardiologist = originalStructuralCardiologist
+    status = originalStatus
+
+    disabled = true
+}
 </script>
 
 <form action={action} method="POST">
     <fieldset class="demographics">
         <label for="mrn">MRN</label>
-        <input type="text" id="mrn" name="mrn" bind:value={mrn}/>
+        <input type="text" id="mrn" name="mrn" bind:value={mrn} {disabled} />
 
         <label for="firstname">First Name</label>
-        <input type="text" id="firstname" name="firstname" bind:value={firstname} />
+        <input type="text" id="firstname" name="firstname" bind:value={firstname} {disabled} />
 
         <label for="lastname">Last Name</label>
-        <input type="text" id="lastname" name="lastname" bind:value={lastname} />
+        <input type="text" id="lastname" name="lastname" bind:value={lastname} {disabled} />
 
         <span class="spacer"></span><span class="spacer"></span>
 
         <label for="sex">Sex</label>
-        <select id="sex" name="sex" bind:value={sex}>
+        <select id="sex" name="sex" bind:value={sex} {disabled}>
             <option value="male">Male</option>
             <option value="female">Female</option>
             <option value="other">Other</option>
         </select>
 
         <label for="dob">DoB</label>
-        <input type="date" id="dob" name="dob" bind:value={dob} />
+        <input type="date" id="dob" name="dob" bind:value={dob} {disabled} />
     </fieldset>
 
     <fieldset class="treating-doctors">
         <label for="general-practitioner">General Practitioner</label>
-        <input type="text" id="general-practitioner" name="generalPractitionerName" bind:value={generalPractitioner} />
+        <input type="text" id="general-practitioner" name="generalPractitionerName" bind:value={generalPractitioner} {disabled} />
 
         <label for="primary-cardiologist">Primary Cardiologist</label>
-        <input type="text" id="primary-cardiologist" name="primaryCardiologistName" bind:value={primaryCardiologist} />
+        <input type="text" id="primary-cardiologist" name="primaryCardiologistName" bind:value={primaryCardiologist} {disabled} />
 
         <label for="structural-cardiologist">Structural Cardiologist</label>
-        <input type="text" id="structural-cardiologist" name="structuralCardiologistName" bind:value={structuralCardiologist} />
+        <input type="text" id="structural-cardiologist" name="structuralCardiologistName" bind:value={structuralCardiologist} {disabled} />
     </fieldset>
 
     <fieldset class="status">
         <label>
-            <input type="radio" name="status" value="assess" bind:group={status} />
+            <input type="radio" name="status" value="assess" bind:group={status} {disabled} />
             <span>Being Assessed</span>
         </label>
         <label>
-            <input type="radio" name="status" value="present" bind:group={status} />
+            <input type="radio" name="status" value="present" bind:group={status} {disabled} />
             <span>Awaiting Presentation</span>
         </label>
         <label>
-            <input type="radio" name="status" value="waitlist" bind:group={status} />
+            <input type="radio" name="status" value="waitlist" bind:group={status} {disabled} />
             <span>On Waitlist</span>
         </label>
         <label>
-            <input type="radio" name="status" value="monitor" bind:group={status} />
+            <input type="radio" name="status" value="monitor" bind:group={status} {disabled} />
             <span>Ongoing Monitoring</span>
         </label>
         <label>
-            <input type="radio" name="status" value="discharge" bind:group={status} />
+            <input type="radio" name="status" value="discharge" bind:group={status} {disabled} />
             <span>Discharged</span>
         </label>
     </fieldset>
 
     <fieldset class="controls">
-        <button type="reset">Reset</button>
-        <button type="submit" class="primary">Save</button>
+        {#if disabled}
+            <button on:click={() => unlock()} class="primary">Unlock</button>
+        {:else}
+            <button type="reset" on:click|preventDefault={() => reset()}>Reset</button>
+            <button type="submit" class="primary">Save</button>
+        {/if}
     </fieldset>
 </form>
 
